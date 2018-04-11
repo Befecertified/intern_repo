47b0d5628ba574b5c5a88d4db296bf6deb1479a7
befecertified <deekor.baribefe@gmail.com>
2018-04-11 09:41:08 +0100

    First commit

---
+++ stageone.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+    <head>
+        <title> Stage One </title>
+        <style>
+            h1 {
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    font-size: 36.3731px;
+    color: #6881A8;
+    text-align: center;
+    margin-top: 100px;
+}
+
+h3 {
+text-align: center;
+font-family: Cambria, Cochin, Georgia, Times, Times New Roman, serif;
+font-style: normal;
+font-weight: normal;
+line-height: normal;
+font-size: 34.2267px;
+color: #515356;
+}
+        </style>
+    </head>
+    <body>
+        <h1>HELLO WORLD</h1>
+        <h3>
+            <?php echo "The time is: ".date('h:i:sa'); ?>
+        </h3>
+    </body>
+</html> 
